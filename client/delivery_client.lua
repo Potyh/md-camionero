4ede5b33fe2c42aa63001024ee9176c240c00d51
Potyh <soy@potyh.es>
2025-01-27 18:24:11 +0100

    NPC de Entrega
    Nuevo sistema de entrega en puerta de casa, animación de entrega y varias funciones agregadas.

--- client/delivery_client.lua
+++ client/delivery_client.lua
@@ -1,7 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local spawnedNPC = nil
 local spawnedDeliveryNPC = nil
-local spawnedVehicle = nil
 local playerVehicle = nil
 local isVehicleAssigned = false
 local isNPCTextShown = false
@@ -9,6 +8,9 @@ local isGarageTextShown = false
 local isOnDuty = false
 local hasDeliveryItem = false
 local currentDelivery = false
+local isNPCOutside = false
+local deliveryStarted = false
+local knockingOnDoor = false
 
 -- Spawnear el NPC principal
 Citizen.CreateThread(function()
@@ -22,50 +24,7 @@ Citizen.CreateThread(function()
     SetEntityInvincible(spawnedNPC, true)
     SetBlockingOfNonTemporaryEvents(spawnedNPC, true)
     FreezeEntityPosition(spawnedNPC, true)
-end)
-
--- Crear el NPC de entrega
-function CreateDeliveryNPC()
-    local npcModel = Config.DeliveryNPC.model
-    RequestModel(npcModel)
-    while not HasModelLoaded(npcModel) do
-        Wait(100)
-    end
-
-    spawnedDeliveryNPC = CreatePed(4, npcModel, Config.DeliveryNPC.coords.x, Config.DeliveryNPC.coords.y, Config.DeliveryNPC.coords.z - 1.0, Config.DeliveryNPC.coords.w, false, true)
-    SetEntityInvincible(spawnedDeliveryNPC, true)
-    SetBlockingOfNonTemporaryEvents(spawnedDeliveryNPC, true)
-    FreezeEntityPosition(spawnedDeliveryNPC, true)
-end
-
--- Eliminar el NPC de entrega
-function DeleteDeliveryNPC()
-    if spawnedDeliveryNPC ~= nil then
-        DeleteEntity(spawnedDeliveryNPC)
-        spawnedDeliveryNPC = nil
-    end
-end
-
--- Manejar el spawn del NPC de entrega
-Citizen.CreateThread(function()
-    while true do
-        Wait(1000)
-        if currentDelivery then
-            local playerCoords = GetEntityCoords(PlayerPedId())
-            local distanceToDeliveryPoint = #(playerCoords - vector3(Config.DeliveryNPC.coords.x, Config.DeliveryNPC.coords.y, Config.DeliveryNPC.coords.z))
-            
-            if distanceToDeliveryPoint < 30.0 and spawnedDeliveryNPC == nil then
-                CreateDeliveryNPC()
-            elseif distanceToDeliveryPoint > 30.0 and spawnedDeliveryNPC ~= nil then
-                DeleteDeliveryNPC()
-            end
-        else
-            if spawnedDeliveryNPC ~= nil then
-                DeleteDeliveryNPC()
-            end
-            Wait(1000)
-        end
-    end
+    SetModelAsNoLongerNeeded(npcModel)
 end)
 
 -- Interacción con el NPC principal
@@ -80,9 +39,8 @@ Citizen.CreateThread(function()
                 exports['qb-core']:DrawText('[E] - Camionero', 'left')
                 isNPCTextShown = true
             end
-            if IsControlJustPressed(0, 38) then -- Tecla "E"
+            if IsControlJustPressed(0, 38) then
                 if not isOnDuty and not isVehicleAssigned then
-                    -- Iniciar trabajo
                     isOnDuty = true
                     SpawnVehicle()
                     TriggerServerEvent('delivery:server:giveStartingItem')
@@ -95,7 +53,7 @@ Citizen.CreateThread(function()
                     if not isVehicleAssigned then
                         isOnDuty = false
                         TriggerServerEvent('delivery:server:givePayout')
-                        QBCore.Functions.Notify('Has terminado tu turno. ¡Buen trabajo!', 'success')
+                        QBCore.Functions.Notify('Has terminado tu turno.', 'success')
                     else
                         QBCore.Functions.Notify('Primero debes devolver el vehículo.', 'error')
                     end
@@ -115,15 +73,18 @@ end)
 -- Verificar la distancia al vehículo
 Citizen.CreateThread(function()
     while true do
-        Wait(1000)
-        if playerVehicle then
+        if playerVehicle and isOnDuty then
             local playerCoords = GetEntityCoords(PlayerPedId())
             local vehicleCoords = GetEntityCoords(playerVehicle)
             local distanceToVehicle = #(playerCoords - vehicleCoords)
 
             if distanceToVehicle > Config.Vehicle.maxDistance then
                 QBCore.Functions.Notify('Tu vehículo está demasiado lejos.', 'error')
+                Wait(5000)
             end
+            Wait(1000)
+        else
+            Wait(5000)
         end
     end
 end)
@@ -176,35 +137,45 @@ Citizen.CreateThread(function()
     end
 end)
 
--- Interacción con el NPC de entrega
+-- Thread para el punto de entrega
 Citizen.CreateThread(function()
-    local isDeliveryTextShown = false
     while true do
         Wait(0)
-        if currentDelivery and spawnedDeliveryNPC then
+        if currentDelivery and not knockingOnDoor then
             local playerCoords = GetEntityCoords(PlayerPedId())
-            local distanceToDelivery = #(playerCoords - vector3(Config.DeliveryNPC.coords.x, Config.DeliveryNPC.coords.y, Config.DeliveryNPC.coords.z))
+            local distanceToDoor = #(playerCoords - vector3(Config.DeliveryPoint.doorCoords.x, Config.DeliveryPoint.doorCoords.y, Config.DeliveryPoint.doorCoords.z))
 
-            if distanceToDelivery < 2.0 then
-                if not isDeliveryTextShown then
-                    exports['qb-core']:DrawText('[E] - Entregar paquete', 'left')
-                    isDeliveryTextShown = true
-                end
-                
+            if distanceToDoor < 2.0 and not deliveryStarted then
+                exports['qb-core']:DrawText('[E] - Llamar a la puerta', 'left')
                 if IsControlJustPressed(0, 38) then
-                    TriggerServerEvent('delivery:server:checkDeliveryItem')
-                end
-            else
-                if isDeliveryTextShown then
+                    deliveryStarted = true
                     exports['qb-core']:HideText()
-                    isDeliveryTextShown = false
+                    
+                    -- Animación de tocar la puerta
+                    TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, true)
+                    QBCore.Functions.Notify('Llamando a la puerta...', 'primary', 3000)
+                    
+                    SetTimeout(3000, function()
+                        ClearPedTasks(PlayerPedId())
+                        MakeNPCComeOut()
+                    end)
+                end
+            elseif isNPCOutside then
+                local distanceToNPC = #(playerCoords - vector3(Config.DeliveryPoint.npcWaitCoords.x, Config.DeliveryPoint.npcWaitCoords.y, Config.DeliveryPoint.npcWaitCoords.z))
+                
+                if distanceToNPC < 2.0 then
+                    exports['qb-core']:DrawText('[E] - Entregar paquete', 'left')
+                    if IsControlJustPressed(0, 38) and hasDeliveryItem then
+                        exports['qb-core']:HideText()
+                        TriggerServerEvent('delivery:server:checkDeliveryItem')
+                    end
                 end
             end
         end
     end
 end)
 
--- Función para generar el vehículo
+-- Funciones
 function SpawnVehicle()
     local vehicleModel = Config.Vehicle.model
     local vehicleCoords = Config.Vehicle.spawnCoords
@@ -214,22 +185,20 @@ function SpawnVehicle()
         Wait(100)
     end
 
-    spawnedVehicle = CreateVehicle(vehicleModel, vehicleCoords.x, vehicleCoords.y, vehicleCoords.z, vehicleCoords.w, true, false)
-    SetVehicleOnGroundProperly(spawnedVehicle)
-    SetEntityAsMissionEntity(spawnedVehicle, true, true)
-    TaskWarpPedIntoVehicle(PlayerPedId(), spawnedVehicle, -1)
-    playerVehicle = spawnedVehicle
-    SetVehicleDoorsLockedForAllPlayers(spawnedVehicle, true)
-    SetVehicleDoorsLockedForPlayer(spawnedVehicle, PlayerId(), false)
+    playerVehicle = CreateVehicle(vehicleModel, vehicleCoords.x, vehicleCoords.y, vehicleCoords.z, vehicleCoords.w, true, false)
+    SetVehicleOnGroundProperly(playerVehicle)
+    SetEntityAsMissionEntity(playerVehicle, true, true)
+    TaskWarpPedIntoVehicle(PlayerPedId(), playerVehicle, -1)
+    SetVehicleDoorsLockedForAllPlayers(playerVehicle, true)
+    SetVehicleDoorsLockedForPlayer(playerVehicle, PlayerId(), false)
+    SetModelAsNoLongerNeeded(vehicleModel)
 
-    TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(spawnedVehicle))
-
+    TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(playerVehicle))
     isVehicleAssigned = true
 end
 
--- Crear blip de entrega
 function CreateDeliveryBlip()
-    local blip = AddBlipForCoord(Config.DeliveryNPC.coords.x, Config.DeliveryNPC.coords.y, Config.DeliveryNPC.coords.z)
+    local blip = AddBlipForCoord(Config.DeliveryPoint.doorCoords.x, Config.DeliveryPoint.doorCoords.y, Config.DeliveryPoint.doorCoords.z)
     SetBlipSprite(blip, 501)
     SetBlipDisplay(blip, 4)
     SetBlipScale(blip, 0.8)
@@ -240,12 +209,119 @@ function CreateDeliveryBlip()
     EndTextCommandSetBlipName(blip)
 end
 
--- Confirmar entrega exitosa
+function MakeNPCComeOut()
+    local npcModel = Config.DeliveryNPC.model
+    RequestModel(npcModel)
+    while not HasModelLoaded(npcModel) do
+        Wait(100)
+    end
+
+    local spawnCoords = Config.DeliveryPoint.npcSpawnCoords
+    spawnedDeliveryNPC = CreatePed(4, npcModel, spawnCoords.x, spawnCoords.y, spawnCoords.z, spawnCoords.w, false, true)
+    
+    SetEntityInvincible(spawnedDeliveryNPC, true)
+    SetBlockingOfNonTemporaryEvents(spawnedDeliveryNPC, true)
+    FreezeEntityPosition(spawnedDeliveryNPC, false)
+    SetModelAsNoLongerNeeded(npcModel)
+
+    local waitCoords = Config.DeliveryPoint.npcWaitCoords
+    ClearPedTasksImmediately(spawnedDeliveryNPC)
+    TaskGoToCoordAnyMeans(spawnedDeliveryNPC, waitCoords.x, waitCoords.y, waitCoords.z, 1.0, 0, 0, 786603, 0xbf800000)
+    
+    Citizen.CreateThread(function()
+        local timeout = 0
+        while timeout < 50 do
+            local npcCoords = GetEntityCoords(spawnedDeliveryNPC)
+            local dist = #(npcCoords - vector3(waitCoords.x, waitCoords.y, waitCoords.z))
+            if dist < 0.5 then
+                SetEntityHeading(spawnedDeliveryNPC, waitCoords.w)
+                break
+            end
+            timeout = timeout + 1
+            Wait(100)
+        end
+        if timeout >= 50 then
+            SetEntityCoords(spawnedDeliveryNPC, waitCoords.x, waitCoords.y, waitCoords.z, false, false, false, false)
+            SetEntityHeading(spawnedDeliveryNPC, waitCoords.w)
+        end
+    end)
+    
+    isNPCOutside = true
+    QBCore.Functions.Notify('Alguien está saliendo...', 'primary')
+end
+
+function MakeNPCGoBack()
+    if spawnedDeliveryNPC then
+        local spawnCoords = Config.DeliveryPoint.npcSpawnCoords
+        ClearPedTasksImmediately(spawnedDeliveryNPC)
+        TaskGoToCoordAnyMeans(spawnedDeliveryNPC, spawnCoords.x, spawnCoords.y, spawnCoords.z, 1.0, 0, 0, 786603, 0xbf800000)
+        
+        QBCore.Functions.Notify('El cliente está regresando a su casa...', 'primary')
+        
+        Citizen.CreateThread(function()
+            local timeout = 0
+            while timeout < 50 and DoesEntityExist(spawnedDeliveryNPC) do
+                local npcCoords = GetEntityCoords(spawnedDeliveryNPC)
+                local dist = #(npcCoords - vector3(spawnCoords.x, spawnCoords.y, spawnCoords.z))
+                if dist < 0.5 then
+                    DeleteEntity(spawnedDeliveryNPC)
+                    spawnedDeliveryNPC = nil
+                    break
+                end
+                timeout = timeout + 1
+                Wait(100)
+            end
+            if timeout >= 50 or DoesEntityExist(spawnedDeliveryNPC) then
+                DeleteEntity(spawnedDeliveryNPC)
+                spawnedDeliveryNPC = nil
+            end
+            isNPCOutside = false
+            deliveryStarted = false
+            knockingOnDoor = false
+        end)
+    end
+end
+
+function CleanupJob()
+    if playerVehicle then
+        DeleteEntity(playerVehicle)
+        playerVehicle = nil
+    end
+    if spawnedDeliveryNPC then
+        DeleteEntity(spawnedDeliveryNPC)
+        spawnedDeliveryNPC = nil
+    end
+    isVehicleAssigned = false
+    isOnDuty = false
+    hasDeliveryItem = false
+    currentDelivery = false
+    isNPCOutside = false
+    deliveryStarted = false
+    knockingOnDoor = false
+    
+    local blip = GetFirstBlipInfoId(501)
+    if blip ~= 0 then
+        RemoveBlip(blip)
+    end
+end
+
+-- Eventos
 RegisterNetEvent('delivery:client:deliveryComplete')
 AddEventHandler('delivery:client:deliveryComplete', function()
     hasDeliveryItem = false
     currentDelivery = false
-    DeleteDeliveryNPC()
     QBCore.Functions.Notify('Entrega completada. Vuelve al punto inicial.', 'success')
     RemoveBlip(GetFirstBlipInfoId(501))
+    MakeNPCGoBack()
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload')
+AddEventHandler('QBCore:Client:OnPlayerUnload', function()
+    CleanupJob()
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        CleanupJob()
+    end
 end)
--- config.lua
+++ config.lua
@@ -1,23 +1,34 @@
 Config = {}
 
+-- Coordenadas del NPC y modelo
 Config.NPC = {
     model = "a_m_y_cyclist_01",
     coords = vector4(984.09, -206.03, 71.07, 237.03),
 }
 
+-- Vehiculo
 Config.Vehicle = {
     model = "burrito3",
     spawnCoords = vector4(978.99, -223.78, 70.01, 332.53),
     maxDistance = 100.0
 }
 
+-- Zona de guardado
 Config.Garage = {
     coords = vector3(988.31, -220.61, 69.93),
 }
 
-Config.DeliveryNPC = {
-    model = "a_m_y_business_02",
-    coords = vector4(998.41, -206.79, 70.51, 119.01),
+-- Punto de entrega
+Config.DeliveryPoint = {
+    doorCoords = vector4(970.93, -199.48, 73.21, 59.56), -- Coordenadas de la puerta
+    npcWaitCoords = vector4(970.7, -199.46, 73.21, 58.42), -- Donde esperará el NPC (un poco alejado de la puerta)
+    npcSpawnCoords = vector4(972.77, -205.64, 73.21, 56.99) -- Donde aparecerá inicialmente el NPC (dentro de la casa)
 }
 
+-- NPC de entrega
+Config.DeliveryNPC = {
+    model = "a_m_y_business_02"
+}
+
+-- Pago por entrega
 Config.Payment = 1000
